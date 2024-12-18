fc088114605d5b171aa4c9444fcc464fb3862473
andreaballard-1988 <dreaball28@gmail.com>
2024-12-17 18:37:48 -0600

    updated js

---
+++ exercise1.js
@@ -0,0 +1,38 @@
+basePizzaCost=10
+toppingCost=1.50
+numberOfToppings=4
+basePizzaCost + (toppingCost*numberOfToppings)
+print total cost
+
+
+console.log("**********")
+let docoration="*"
+let decoration2 -"o-"
+
+console.log(decoration.repeat(8))-->*********
+console.log(decoration2.repeast(5))-->o-o-o-o-o-
+
+
+
+
+//start Pseudocode:
+//base pizza cost equals 10
+    const basePizzaCost=10;
+
+// number of toppings is 4
+    numberOfToppings >= 4;
+
+// topping costs is 1.50
+    toppingCost=1.50;
+
+//print total cost of pizza
+    totalCost= (basePizzacost + toppingCost(numberofToppings));
+
+//end
+
+function:
+
+updatePizza: totalCost=(basePizzaCost + numberOfToppings*cost)
+
+
+
---
+++ exercise3.js
@@ -0,0 +1,17 @@
+/***********
+ * coffee shop calculator
+each type of coffee has a fixed Price 
+calculate total cost of ordering multiple drinks
+include a 10% Tax 
+assume you order one latte one espresso and one cappuccino
+************/
+
+
+let latte = 5;
+let espresso = 6;
+let cappuccino = 7;
+let totalCost = (latte*0.10 + espresso*0.10 + cappuccino*0.10);
+console.log(latte + latte*0.10)
+console.log(espresso + espresso*0.10);
+console.log(cappuccino + cappuccino*0.10);
+console.log(totalCost)
